f4390e2740a86af804c3927c7f88efdb9d354669
Alex Degregori <alexdegregori@gmail.com>
2022-06-23 16:44:04 -0700

    Added axios requests

--- index.js
+++ index.js
@@ -1,18 +1,124 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // Parses JSON bodies
-const cookieParser = require("cookie-parser");
 const app = express().use(bodyParser.text());
 const port = process.env.PORT || 3000;
 const axios = require("axios"); // Helps by sending HTTP for us
 
+// GET route to authenticate the Productboard webhook
 app.get("/productboard-webhook", async (req, res) => {
 	res.setHeader("Content-type", "text/plain");
 	res.status(200).send(res.req.query.validationToken);
 });
 
+// POST route to handle Productboard webhook updates
 app.post("/productboard-webhook", async (req, res) => {
-	console.log("Req is:", req);
-	console.log("Res is:", res);
+	// Grabbing feature ID from the webhook POST
+	const productboardFeatureID = req.body.data.id;
+	// 🛑 Add your Jira integration ID below. You can access the ID by going to your Jira integration settings and copying the string in the final slash
+	const jiraIntegrationId = "feac0a2e-8600-4fac-876d-964c5f15f911";
+	// 🛑 Add your PB API token below. More info on creating an API token here: https://developer.productboard.com/#section/Authentication/Getting-a-token
+	const pbAPITokenID =
+		"eyJ0eXAiOiJKV1QiLCJraWQiOiJlY2IzMmI3MjdiNGY5NWFiOTkzNWNlMjhjYWViZGQ0MGRhYzIzMDk2YTJhZjliMDU1ZmJkZGEwOGM0ZmZiMzNmIiwiYWxnIjoiUlM1MTIifQ.eyJpc3MiOiJjNjU2YjMyNC04NmRjLTQ0ZWQtOWViNy1mMGYwMDEzMDhlMGEiLCJzdWIiOiI5NzEwMyIsInJvbGUiOiJhZG1pbiIsImF1ZCI6Imh0dHBzOi8vYXBpLnByb2R1Y3Rib2FyZC5jb20iLCJ1c2VyX2lkIjo5NzEwMywic3BhY2VfaWQiOiI1Nzc4MSIsImlhdCI6MTYzODIwNTEzN30.H4K0MNtRUeNLyClaEuHMp1UY8lUTgrR8QBfaVD8uyuEAtk9U-y9uuWb0m0CpJuLUm9bc3fSanFc8_-by9OgT2WERJT0UjgmH2RbXxN-te8tptsw2kdgbUqFNIMP2wqpXkIvwamdIwxtJP3Tj07BV0NpuoSGBLoppSNelg2yOWgOM9vtnrjHZx1V94lAJde9-bXo092wFaRMk8QcdTu-AyY-4Ao_x4h6p5d1Yzf1_L7qb7Royk7YhpAKySUK0B2noShlFzLu9roPnYwO8GT7EEFE5OtKco4sURYDXDULZbtyJE1Ztr_dY6W4PI9D2kssDo6cIYVK_AsoT51CWzvhKWw";
+	let jiraPBStatusMap;
+
+	// Configuration for GET request to grab feature status
+	const getFeatureInfoConfig = {
+		method: "get",
+		url: `https://api.productboard.com/features/${productboardFeatureID}`,
+		headers: {
+			"X-Version": "1",
+			Authorization: `Bearer ${pbAPITokenID}`,
+		},
+	};
+
+	// Configuration for GET request to grab the Jira ID the PB feature is connected to
+	const getPbJiraInfoConfig = {
+		method: "get",
+		url: `https://api.productboard.com/jira-integrations/${jiraIntegrationId}/connections/${productboardFeatureID}`,
+		headers: {
+			"X-Version": "1",
+			Authorization: `Bearer ${pbAPITokenID}`,
+		},
+	};
+
+	// GET request is sent to grab feature status
+	axios(getFeatureInfoConfig)
+		.then(function (response) {
+			console.log("Feature status is", response.data.data.status.name);
+			const pbFeatureStatus = response.data.data.status.name;
+
+			// 🛑 Add PB <> Jira Status mappings below. The variable pbFeatureStatus is the status name of the feature and the jiraPBStatusMap variable represents the Jira status.
+			// Note that a transistion ID is provided to you for each Jira Status. In order to update a Jira issue's status via the API, you must send the Transistion ID in the body of your request.
+			// To easily find your Transition IDs, make a GET request to one of your issues i.e. https://<Enter Jira URL>.atlassian.net/rest/api/3/issue/<Enter Jira ID here>/transitions
+			// The response will show you what the Transition IDs are for each Jira status
+
+			if (pbFeatureStatus == "Candidate") {
+				jiraPBStatusMap = {
+					transition: {
+						id: "11",
+					},
+				};
+			} else if (pbFeatureStatus == "In Progress") {
+				jiraPBStatusMap = {
+					transition: {
+						id: "21",
+					},
+				};
+			} else if (pbFeatureStatus == "Released") {
+				jiraPBStatusMap = {
+					transition: {
+						id: "41",
+					},
+				};
+			} else {
+				jiraPBStatusMap = null;
+			}
+
+			console.log("Assigned PB status is", pbFeatureStatus);
+			console.log("Transition ID is", jiraPBStatusMap);
+
+			// Request to find Jira issue ID the PB feature is connected to
+			axios(getPbJiraInfoConfig)
+				.then(function (response) {
+					console.log(
+						"Feature Jira ID is",
+						response.data.data.connection.issueId
+					);
+
+					// Get Jira integration ID, Transition ID, and configure the request to update Jira status
+					const pbFeatureJiraId = response.data.data.connection.issueId;
+					const transitionData = JSON.stringify(jiraPBStatusMap);
+					const jiraConfig = {
+						method: "post",
+						url: `https://excellence-alex.atlassian.net/rest/api/3/issue/${pbFeatureJiraId}/transitions`,
+						headers: {
+							Authorization:
+								"Basic YWxleC5kZWdyZWdvcmlAcHJvZHVjdGJvYXJkLmNvbTpnN09OY3Y5YllPNkFmeUs1SWUzQ0EzRjQ=",
+							"Content-Type": "application/json",
+							Cookie:
+								"atlassian.xsrf.token=7f6f5a2e-5693-4d95-98b9-1f1c05ef1a5a_72445b831692bfd97ef1fb4b0a1b350a558f46c4_lin",
+						},
+						data: transitionData,
+					};
+
+					// Send request to Jira to update status
+					axios(jiraConfig)
+						.then(function (response) {
+							// Confirm status update went through with response code 204
+							console.log("Jira response code is:", response.status);
+						})
+						.catch(function (error) {
+							console.log(error);
+						});
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+	res.end();
 });
 
 app.listen(port, () => {
